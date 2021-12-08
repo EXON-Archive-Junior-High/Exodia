5740b14cad1c6a11096fad9ac673ba7cd68d4426
시혁 박 <exon.indiv@gmail.com>
2021-12-08 19:03:10 +0900

    Add: getUser

--- src/getUser.ts
+++ src/getUser.ts
@@ -1,5 +1,19 @@
 import fetch from 'node-fetch'
-import { readJSONSync } from 'fs-extra'
 
-const get = async (url: string) => await (await fetch(url)).json()
+interface IDictionary<TValue> {
+    [id: string]: TValue;
+}
 
+//const get = async (url: string) => await (await fetch(url)).json()
+const rank2int: IDictionary<number> = {'I': 400, 'II': 300, 'III': 200, 'IV': 100}
+const tier2int: IDictionary<number> = {'BRONZE': 1000, 'SILVER': 2000, 'GOLD': 3000, 'PLATINUM': 4000, 'DIAMOND': 5000, 'MASTER': 6000, 'GRANDMASTER': 7000, 'CHALLENGER': 8000}
+
+export default async function getUser(users: string[]) {
+    for (const user of users) {
+        const { id } = await (await fetch(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(user)}?api_key=${process.env.api_key}`)).json()
+        let profiles = await (await fetch(`https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${process.env.api_key}`)).json()
+
+        const point = ~profiles[0].leaguePoints + rank2int[profiles[0].rank] + tier2int[profiles[0].tier]
+        console.log(user, point, profiles[0].leaguePoints, profiles[0].rank, profiles[0].tier)
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
+import path from 'path'
+import { readJSONSync } from 'fs-extra'
 import { Client, Intents } from 'discord.js'
 
+import getUser from './getUser'
+
+const PATH = path.resolve()
+
+const { users }: any = readJSONSync(PATH + '/settings.json')
 export const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
-client.on('ready', async () => console.log('[*] Ready'))
+client.on('ready', async () => {
+    console.log('[*] Ready')
+    client.user?.setActivity('\"!랭킹\" 를 입력하세요', { type: 'LISTENING' })
+    getUser(users)
+})
 
 client.on('message', async (msg) => {
-    if (msg.content === 'ㅎㅇ') msg.channel.send('d')
+    if (msg.content === '!랭킹') {
+        // console.log(users)
+        // getUser(users)
+        msg.channel.send('d')
+    }
 })
 
 client.login(process.env.Token)
