293623f38b329e0d6c42d08c9f29c18da63896f1
시혁 박 <exon.indiv@gmail.com>
2021-12-14 20:02:28 +0900

    Bug: solo ranking

--- src/getUser.ts
+++ src/getUser.ts
@@ -11,7 +11,7 @@ interface IUser {
 }
 
 const { api_key } = require('../settings.json')
-//const get = async (url: string) => await (await fetch(url)).json()
+
 const rank2int: IDictionary<number> = {'I': 303, 'II': 202, 'III': 101, 'IV': 0}
 const tier2int: IDictionary<number> = {'IRON': 0, 'BRONZE': 1000, 'SILVER': 2000, 'GOLD': 3000, 'PLATINUM': 4000, 'DIAMOND': 5000, 'MASTER': 6000, 'GRANDMASTER': 7000, 'CHALLENGER': 8000}
 
@@ -23,9 +23,13 @@ export default async function getUser(users: string[]) {
 
         let points = 0
         let tier = '배치 안봄'
-        if (profiles.length) {
-            points = ~profiles[0].leaguePoints + rank2int[profiles[0].rank] + tier2int[profiles[0].tier]
-            tier = `${profiles[0].tier} ${profiles[0].rank} ${profiles[0].leaguePoints}`
+        for (const profile of profiles) {
+            if ((profile.queueType === 'RANKED_SOLO_5x5') && profiles.length) {
+                console.log(profile)
+                points = ~profiles[0].leaguePoints + rank2int[profiles[0].rank] + tier2int[profiles[0].tier]
+                tier = `${profiles[0].tier} ${profiles[0].rank} ${profiles[0].leaguePoints}`
+                break
+            }
         }
         ranking.push({ name: user, value: points, tier: tier})
     }
--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,7 @@ export const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
 client.once('ready', async () => {
     console.log('[*] Ready')
-    client.user?.setActivity('\"!랭킹\" 를 입력하세요', { type: 'LISTENING' })
-    getUser(users)
+    client.user?.setActivity('\"/랭킹\" 를 입력하세요', { type: 'LISTENING' })
 })
 
 client.on('interactionCreate', async interaction => {
@@ -20,15 +19,18 @@ client.on('interactionCreate', async interaction => {
         const ranking = getUser(users)
         const embed = new MessageEmbed({
             title: '엑조디아 랭킹',
+            color: 'GREEN',
             description: '엑조디아 멤버들',
             timestamp: new Date(),
 	        footer: {
 		        text: 'Exodia'
 	        },
         })
+        let i: number = 1
         for (const user of await ranking) {
             console.log(user)
-            embed.addField(user.name, user.tier)
+            embed.addField(`${i}등 ${user.name}`, user.tier)
+            i++
         }
 		await interaction.reply({ embeds: [embed] })
 	}
